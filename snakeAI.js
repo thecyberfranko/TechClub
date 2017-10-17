3f30c48f5214ca5685d1ccec0b0af9e886fe7a15
JonathanNiehenke <jrniehenke@gmail.com>
2017-10-16 20:19:21 -0700

    Improve pathing into individual segments

--- snakeAI.js
+++ snakeAI.js
@@ -1,114 +1,81 @@
 function snakeAI_Obj() {
-    this.game, this.snake, this.path = { "next": function() { return false; } };
-    this.toPoint = function(Src, Dest, Horizontal) {
-        if (Horizontal) {
+    this.game, this.snake, this.path = [];
+    this.orthogonalToPoint = function(Src, Dest, Horizontal) {
+        if (Horizontal) {  // Favor east/west movement then north/south
             if (Src.x < Dest.x) return this.game.directions["east"];
             if (Src.x > Dest.x) return this.game.directions["west"];
             if (Src.y < Dest.y) return this.game.directions["south"];
             if (Src.y > Dest.y) return this.game.directions["north"];
         }
-        else {
+        else {  // Favor north/south movement then east/west
             if (Src.y < Dest.y) return this.game.directions["south"];
             if (Src.y > Dest.y) return this.game.directions["north"];
             if (Src.x < Dest.x) return this.game.directions["east"];
             if (Src.x > Dest.x) return this.game.directions["west"];
         }
+        throw "On target";
     };
-    this.getPathFeatures = function(i) {
-        let Horizontal = false, head = this.snake.head, Points, outside;
-        switch (i) {
-            case 0:
-                Horizontal = true;
-            case 1:
-                Points = [head, this.game.food, this.snake.body[0]];
-                break;
-            case 2:
-                Horizontal = true;
-                outside = new PointObj(
-                    (head.x < this.snake.body[0].x ? 0
-                        : this.game.canvas.width - this.game.tileSize),
-                    head.y);
-                Points = [head, this.game.food, outside];
-                break;
-            case 3:
-                outside = new PointObj(head.x,
-                    (head.y < this.snake.body[0].y ? 0
-                        : this.game.canvas.height - this.game.tileSize));
-                Points = [head, this.game.food, outside];
-                break;
-            case 4:
-                Horizontal = true;
-            case 5:
-                Points = [head, this.snake.body[0]];
-                break;
-            case 6:
-                Horizontal = true;
-                outside = new PointObj(
-                    (head.x < this.snake.body[0].x ? 0
-                        : this.game.canvas.width - this.game.tileSize),
-                    head.y);
-                Points = [head, outside];
-                break;
-            case 7:
-                outside = new PointObj(head.x,
-                    (head.y < this.snake.body[0].y ? 0
-                        : this.game.canvas.height - this.game.tileSize));
-                Points = [head, outside];
-                break;
-            case 8:
-                outside = new PointObj(0, 0);
-                Points = [head, outside];
-                break;
-            case 9:
-                outside = new PointObj(
-                    this.game.canvas.width - this.game.tileSize,
-                    this.game.canvas.height - this.game.tileSize);
-                Points = [head, outside];
-                break;
-            case 10:
-                outside = new PointObj(
-                    0, this.game.canvas.height - this.game.tileSize);
-                Points = [head, outside];
-                break;
-            case 11:
-                outside = new PointObj(
-                    this.game.canvas.width - this.game.tileSize, 0);
-                Points = [head, outside];
-                break;
-        }
-        return {"Points": Points, "Horizontal": Horizontal};
-    };
-    this.getSeqAlong = function(Points, Horizontal) {
+    this.getSeqAlong = function(Point, Dest, Horizontal) {
         let Path = [];
-        for (i = 0; i < Points.length - 1; ++i) {
-            let Point = Points[i], Dest = Points[i + 1];
-            while (!Dest.equals(Point)) {
-                heading = this.toPoint(Point, Dest, Horizontal);
-                Point = Point.add(heading);
-                Path.push(Point);
-            }
+        while (!Point.equals(Dest)) {
+            heading = this.orthogonalToPoint(Point, Dest, Horizontal);
+            Point = Point.add(heading);
+            Path.push(Point);
         }
         return Path;
     };
-    this.isBadPath = function(Path) {
+    this.isSafe = function(Path) {
         for (Point of Path) {
             if (this.snake.isBody(Point) && !Point.equals(this.snake.body[0]))
-                return true;
+                return false;
         }
-        return false;
+        return true;
+    };
+    this.getSegment = function(Src, Dest) {
+        for (let i = 0; i < 2; ++i) {
+            let Segment = this.getSeqAlong(Src, Dest, i);
+            if (this.isSafe(Segment))
+                return Segment;
+        }
+        return [];
+    };
+    this.getOutsidePath = function(Source) {
+        return [];
+    };
+    this.getSafePath = function(Source) {
+        let head = this.snake.head, tail = this.snake.body[0], Detour = [];
+        let pathToTail = this.getSegment(Source, tail);
+        for (let i = 0; !pathToTail.length && i < 3; ++i) {
+            let Segment = this.getOutsidePath(Source, i);
+            if (!Segment.length) {
+                Detour = [];
+                break;
+            }
+            Detour.concat(Segment);
+            pathToTail = this.getSegment(Segment[Segment.length-1], tail);
+        }
+        if (!pathToTail.length)
+            Detour = [];
+        return Detour.concat(pathToTail);
+    };
+    this.getPath = function() {
+        let foodPath = this.getSegment(this.snake.head, this.game.food);
+        if (foodPath.length) {
+            let safePath = this.getSafePath(this.game.food);
+            if (safePath.length)  // Safe to get food
+                return foodPath;
+        }
+        let Path = this.getSafePath(this.snake.head)
+        console.log(typeof(this.path), this.path)
+        return Path;
     };
     this.loop = function() {
         this.game.loop();
         if (!this.path.length) {
-            let i = 0, Features, Path;
-            do {
-                Features = this.getPathFeatures(i);
-                Path = this.getSeqAlong(Features.Points, Features.Horizontal);
-            } while (i++ < 11 && (Path.length == 0 || this.isBadPath(Path)));
-            this.path = this.getSeqAlong(
-                Features.Points.slice(0, 2), Features.Horizontal);
+            this.path = this.getPath();
         }
-        this.game.heading = this.path.shift().sub(this.snake.head);
+        try { this.game.heading = this.path.shift().sub(this.snake.head); }
+        catch (e) { console.log(typeof(this.path), this.path)}
     }
     this.init = function() {
         this.game = new GameObj(document.querySelector("CANVAS"), 30, 20, 20);
@@ -118,7 +85,7 @@ function snakeAI_Obj() {
     this.beginLoop = function() {
         this.game.animation = setInterval(this.loop.bind(this), 50);
     }
-}
+};
 
 function init() {
     let snakeAI = new snakeAI_Obj();
